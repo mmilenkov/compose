f622c02684c419c0284acbbc5034dc2318b4d356
Miloslav G. Milenkov <mgmilenkov@gmail.com>
2022-01-30 17:16:53 +0200

    Messing around.

--- app/src/main/java/org/selostudios/compose/MainActivity.kt
+++ app/src/main/java/org/selostudios/compose/MainActivity.kt
@@ -3,28 +3,46 @@ package org.selostudios.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import org.selostudios.compose.ui.theme.ComposeSampleTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            //SampleColumn()
+            /*val painter = painterResource(id = R.drawable.ic_kotlin)
+            ImageCard(
+                painter = painter,
+                contentDescription = "Kotlin Icon",
+                title = "Kotlin Icon"
+            )
+            */
+            //TextModifiers(text = "This is some random text to test with")
         }
     }
 }
@@ -34,10 +52,122 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun TextModifiers(text: String) {
+    val fontFamily = FontFamily(Font(R.font.comforter_regular, FontWeight.Normal))
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color(0xFF101010))) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontSize = 25.sp
+                    )
+                ) {
+                    append("Hello, is it me you're looking for?")
+                    append(" ")
+                }
+                append(text)
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontSize = 25.sp
+                    )
+                ) {
+                    append(" ")
+                    append("I can see it in your eyes")
+                }
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
+            textDecoration = TextDecoration.Underline,
+
+        )
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 2.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(6.dp),
+            contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SampleColumn() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(2.dp)
+        .border(1.dp, Color.Black)
+        .padding(2.dp)
+        .border(1.dp, Color.Red)
+        .padding(2.dp)
+        .border(1.dp, Color.Green)
+        .padding(2.dp)
+        .border(1.dp, Color.Blue)
+        .background(Color.LightGray, RectangleShape),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Greeting("Android")
+        Spacer(Modifier.height(30.dp))
+        Greeting("Kotlin")
+        Text(text = "Hello Compose", modifier = Modifier.offset(20.dp, 10.dp))
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeSampleTheme {
-        Greeting("Android")
+    //SampleColumn()
+    /*val painter = painterResource(id = R.drawable.ic_kotlin)
+    Box(modifier = Modifier.fillMaxSize(0.5f)) {
+        ImageCard(
+            painter = painter,
+            contentDescription = "Kotlin Icon",
+            title = "Kotlin Icon"
+        )
     }
+     */
+    //TextModifiers(text = "This is some random text to test with")
 }
